65ad2838d1e597423b9b264854a3563e6e7d937f
unknown <imubashirhussen@gmail.com>
2026-07-01 17:44:06 +0500

    Added a updated Service layer CategoryService

--- src/main/java/com/mubashar/expensetracker/service/CategoryService.java
+++ src/main/java/com/mubashar/expensetracker/service/CategoryService.java
@@ -1,4 +1,79 @@
-package com.mubashar.expensetracker.controller;
+package com.mubashar.expensetracker.service;
 
+import com.mubashar.expensetracker.dto.CategoryRequest;
+import com.mubashar.expensetracker.dto.CategoryResponse;
+import com.mubashar.expensetracker.entity.Category;
+import com.mubashar.expensetracker.exceptions.CategoryNotFoundException;
+import com.mubashar.expensetracker.repository.CategoryRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepo categoryRepo;
+
+    public CategoryService(CategoryRepo categoryRepo){
+        this.categoryRepo = categoryRepo;
+    }
+
+    public Category saveCategory(CategoryRequest categoryReq) {
+
+    Category category = new Category();
+    category.setName(categoryReq.name());
+    category.setUserId(categoryReq.userId());
+
+        String categoryName = categoryReq.name().toUpperCase();
+
+        if (categoryName.equals("FOOD") || categoryName.equals("HEALTH") || categoryName.equals("TRAVEL") || categoryName.equals("SHOPPING")) {
+
+            return categoryRepo.save(category);
+
+        } else {
+            throw new CategoryNotFoundException(("Category Not Found") + categoryReq.name());
+        }
+
+    }
+
+    public List<CategoryResponse> allCategories() {
+        List<Category> allCategory = categoryRepo.findAll() ;
+
+        List<CategoryResponse> categoryResponses = new ArrayList<>();
+        for(Category  category: allCategory){
+
+            categoryResponses.add(new CategoryResponse(category.getName(),category.getUserId()));
+        }
+        return  categoryResponses;
+
+    }
+
+    public Category searchCategoryById(Long id) {
+        return categoryRepo.findById(id).orElseThrow(()-> new CategoryNotFoundException("Category Not Found" + id));
+    }
+
+    public CategoryResponse updateCategory(Long id, CategoryRequest categoryReq) {
+
+         categoryRepo.findById(id).orElseThrow(()-> new CategoryNotFoundException("Id not Exist"+ id));
+
+        Category category1 = new Category();
+        //category1.setId(categoryReq.Id());
+        category1.setName(categoryReq.name());
+        category1.setUserId(categoryReq.userId());
+
+        return new CategoryResponse(category1.getName(), category1.getUserId());
+
+    }
+
+    public void deleteCategory(Long id) {
+
+        if(categoryRepo.existsById(id)){
+
+            categoryRepo.deleteById(id);
+        }
+        else{
+            throw  new CategoryNotFoundException("Category not found");
+        }
+    }
 }
