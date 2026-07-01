44cc486c50bb5e7aa4d1a89b06720cf583a796b1
unknown <imubashirhussen@gmail.com>
2026-07-01 17:48:11 +0500

    Added a Custom Exception

--- src/main/java/com/mubashar/expensetracker/exceptions/CategoryNotFoundException.java
+++ src/main/java/com/mubashar/expensetracker/exceptions/CategoryNotFoundException.java
@@ -1,4 +1,9 @@
 package com.mubashar.expensetracker.exceptions;
 
-public class CategoryNotFoundException {
+public class CategoryNotFoundException extends RuntimeException{
+
+    public CategoryNotFoundException(String message){
+
+        super(message);
+    }
 }
