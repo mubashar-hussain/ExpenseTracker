c95ea6458aff18d05d85d1ff0daf5a04b98fe5a1
unknown <imubashirhussen@gmail.com>
2026-07-01 17:38:28 +0500

    added a repo layer CategoryRepo

--- src/main/java/com/mubashar/expensetracker/repository/CategoryRepo.java
+++ src/main/java/com/mubashar/expensetracker/repository/CategoryRepo.java
@@ -1,4 +1,7 @@
-package com.mubashar.expensetracker.service;
+package com.mubashar.expensetracker.repository;
 
-public interface CategoryRepo {
+import com.mubashar.expensetracker.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepo extends JpaRepository<Category,Long> {
 }
