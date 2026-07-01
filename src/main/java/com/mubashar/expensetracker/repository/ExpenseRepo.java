1e919a483882f9471f52ea9244872ed8f461f1aa
unknown <imubashirhussen@gmail.com>
2026-07-01 17:42:22 +0500

    ExpenseRepo

--- src/main/java/com/mubashar/expensetracker/repository/ExpenseRepo.java
+++ src/main/java/com/mubashar/expensetracker/repository/ExpenseRepo.java
@@ -1,7 +1,10 @@
-package com.mubashar.expensetracker;
+package com.mubashar.expensetracker.repository;
 
+import com.mubashar.expensetracker.entity.Category;
 import com.mubashar.expensetracker.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseRepo extends JpaRepository<Expense,Long> {
+
+    //Expense save(Expense expense, Category category);
 }
