bae7245efa9b58359c45a1dcabb2c215ece56e50
unknown <imubashirhussen@gmail.com>
2026-07-01 17:48:49 +0500

    Added ExpenseNotFoundException

--- src/main/java/com/mubashar/expensetracker/exceptions/ExpenseNotFoundException.java
+++ src/main/java/com/mubashar/expensetracker/exceptions/ExpenseNotFoundException.java
@@ -1,4 +1,8 @@
 package com.mubashar.expensetracker.exceptions;
 
-public class ExpenseNotFoundException {
+public class ExpenseNotFoundException extends  RuntimeException{
+
+    public ExpenseNotFoundException(String message){
+        super(message);
+    }
 }
