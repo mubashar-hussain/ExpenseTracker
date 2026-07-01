8b2cdeb55d7f9b5540cf6d2007e6acd4ad935955
unknown <imubashirhussen@gmail.com>
2026-07-01 16:45:03 +0500

    added a ExpenserController

--- src/main/java/com/mubashar/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/mubashar/expensetracker/controller/ExpenseController.java
@@ -1,8 +1,14 @@
 package com.mubashar.expensetracker.controller;
 
+import com.mubashar.expensetracker.dto.ExpenseRequest;
+import com.mubashar.expensetracker.dto.ExpenseResponse;
+import com.mubashar.expensetracker.entity.Expense;
 import com.mubashar.expensetracker.service.ExpenseService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -11,6 +17,55 @@ public class ExpenseController {
     private final ExpenseService expenseService;
 
     public ExpenseController(ExpenseService expenseService) {
+
         this.expenseService = expenseService;
     }
+
+    @PostMapping("/addExpense")
+    public ResponseEntity<ExpenseResponse> addExpense(@RequestBody ExpenseRequest expenseRequest){
+
+        Expense expense = expenseService.addExpense(expenseRequest);
+
+         ExpenseResponse expenseResponse = new ExpenseResponse(expense.getAmount(),expense.getTitle(),expense.getLocalDate(),expense.getDescription(),expense.getCategory());
+
+         return ResponseEntity
+                 .status(HttpStatus.CREATED)
+                 .body(expenseResponse);
+    }
+
+    @GetMapping("/allExpense")
+    public ResponseEntity<List<ExpenseResponse>> getAllExpense(){
+
+        List<ExpenseResponse> expenseList = expenseService.getAllExpense();
+
+        return ResponseEntity.ok(expenseList);
+
+    }
+
+    @GetMapping("/expense/{id}")
+    public ResponseEntity<ExpenseResponse> getExpenseById(@PathVariable Long id){
+        ExpenseResponse expenseResponse = expenseService.expenseById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(expenseResponse);
+
+    }
+
+     @PutMapping("/expense/{id}")
+    public ResponseEntity<ExpenseResponse> updateExpense(@PathVariable Long id, @RequestBody ExpenseRequest expenseRequest){
+
+        ExpenseResponse expenseResponse = expenseService.updateExpense(id, expenseRequest);
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(expenseResponse);
+
+    }
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<Void> deleteExpense(@PathVariable Long id){
+
+        expenseService.deleteExpense(id);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    }
+
+
 }
