5ca02f920824f06840a573ced1505a20f5aecdc9
unknown <imubashirhussen@gmail.com>
2026-07-05 23:20:56 +0500

    addeda findByTitle method in expense repo

--- src/main/java/com/mubashar/expensetracker/repository/ExpenseRepo.java
+++ src/main/java/com/mubashar/expensetracker/repository/ExpenseRepo.java
@@ -1,10 +1,13 @@
 package com.mubashar.expensetracker.repository;
 
-import com.mubashar.expensetracker.entity.Category;
+import com.mubashar.expensetracker.dto.ExpenseResponse;
 import com.mubashar.expensetracker.entity.Expense;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExpenseRepo extends JpaRepository<Expense,Long> {
 
-    //Expense save(Expense expense, Category category);
+public interface ExpenseRepo extends JpaRepository<Expense,Long> {
+    Page<ExpenseResponse> findByTitle(String search, Pageable pageable);
+
 }
