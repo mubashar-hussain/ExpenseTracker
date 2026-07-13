b1e3f82e4558d96533173ece8c070d1f09c25131
unknown <imubashirhussen@gmail.com>
2026-07-13 12:23:52 +0500

    added a custem query for sumof all amout

--- src/main/java/com/mubashar/expensetracker/repository/ExpenseRepo.java
+++ src/main/java/com/mubashar/expensetracker/repository/ExpenseRepo.java
@@ -5,9 +5,17 @@ import com.mubashar.expensetracker.entity.Expense;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
 
 
 public interface ExpenseRepo extends JpaRepository<Expense,Long> {
     Page<ExpenseResponse> findByTitle(String search, Pageable pageable);
+
+    @Query("SELECT COALESCE(SUM(e.amount), 0) FROM Expense e")
+    BigDecimal sumAllAmounts();
+
+
 
 }
