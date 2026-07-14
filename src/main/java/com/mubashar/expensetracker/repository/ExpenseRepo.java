7579259b8818b2efbcf281ae21b2ded262c10bfe
unknown <imubashirhussen@gmail.com>
2026-07-14 22:59:23 +0500

    added a total monthly expense range

--- src/main/java/com/mubashar/expensetracker/repository/ExpenseRepo.java
+++ src/main/java/com/mubashar/expensetracker/repository/ExpenseRepo.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 
 public interface ExpenseRepo extends JpaRepository<Expense,Long> {
@@ -15,6 +17,11 @@ public interface ExpenseRepo extends JpaRepository<Expense,Long> {
 
     @Query("SELECT COALESCE(SUM(e.amount), 0) FROM Expense e")
     BigDecimal sumAllAmounts();
+
+    @Query("select coalesce(SUM(e.amount),0) FROM Expense e WHERE e.localDate BETWEEN :from AND :to")
+    BigDecimal sumAmountBetween(@Param("from") LocalDate from, @Param("to")LocalDate to);
+
+
 
 
 
